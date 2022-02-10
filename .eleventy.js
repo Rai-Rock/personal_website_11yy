b77cc46207d789ba228d40fdb8b1b7ac8b12f53f
Riccardo Gualtieri <riccardogualtieribusiness@gmail.com>
2022-02-10 14:31:21 +0100

    final commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,16 @@
+const {DateTime} = require("luxon");
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/styles/main.css');
+  eleventyConfig.addPassthroughCopy('.src/assets');
+  
+  eleventyConfig.addFilter("postDate",(dateObj)=>{
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
   return {
-    dir:{
-      
+    dir: {
+      input:"src",
+      output: "public",
+      scripts:"scripts"
     }
-  }
-}
+  };
+};
