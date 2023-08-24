bc660f9ec8c790f519fc6dfc097ad969b0ce7939
Riccardo Gualtieri <riccardogualtieribusiness@gmail.com>
2023-08-24 16:48:39 +0200

    style changes
    main page style changes

---
+++ src/scripts/date.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+var data = new Date();
+var gg, mm, aaaa;
+gg = data.getDate() + "/";
+mm = data.getMonth() + 1 + "/";
+aaaa = data.getFullYear();
+document.write("Oggi è il " + gg + mm + aaaa);
+
+</script>
